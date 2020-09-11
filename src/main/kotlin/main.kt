0bcb3a2d1b64f36c7a8b0a1c2408a949580315ca
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2020-09-11 16:57:08 -0500

    Update gitignore, add more Kotlin examples of code smells and bugs

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,3 +5,22 @@ fun main() {
     println("Hello, $name!")
 //    println("Hello World!")
 }
+
+fun vulnerableFunction() {
+    val password = "password" // Vulnerability - hardcoded password
+    if (!password.isBlank())println("null password!")
+}
+
+// Code Smell - Empty function
+fun emptyFunction() {
+}
+
+fun buggyFunction(str: String){
+    if (str == "hello"){
+        println("Hello!")
+    } else if (str == "goodbye"){
+        println("Goodbye!")
+    } else if (str == "hello"){ // Bug - Duplicate condition
+        println("Hello again!")
+    }
+}
