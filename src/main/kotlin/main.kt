3f38fbd1cbb874dc98773c8f53d5d9f3da0fa303
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2022-02-14 11:47:39 -0600

    Add vulnerability

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
 fun main() {
 //    println("Hello World!")
     println("What's your name?")
@@ -24,3 +29,32 @@ fun buggyFunction(str: String){
         println("Hello again!")
     }
 }
+
+// Trust All Cert
+// Create a trust manager that does not validate certificate chains
+val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+    override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+        throw UnsupportedOperationException("checkClientTrusted: Not supported yet.");
+    }
+
+    override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+        require(chain != null) {
+//            d("Certificate is null or empty")
+        }
+        require(!(authType == null || authType.isEmpty())) {
+//            d("Auth type is null or empty")
+        }
+        try {
+            chain?.firstOrNull()?.checkValidity()
+        } catch (e: Exception) {
+//            Helper.logExceptionOnFirebase(e)
+            throw CertificateException("Certificate is not valid or trusted")
+        }
+
+    }
+
+    override fun getAcceptedIssuers(): Array<X509Certificate> {
+        return arrayOf()
+    }
+})
+
